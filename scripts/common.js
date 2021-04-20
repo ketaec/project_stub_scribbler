c90fdda12eee431978bb9eae4898ee283bb196fc
KetaEs <eswar-chand.keta@basf.com>
2021-04-20 10:42:40 +0200

    implemented close button and signin logics

--- scripts/common.js
+++ scripts/common.js
@@ -5,6 +5,11 @@ var signInModal = document.getElementById("signin-modal");
 // Get the buttons
 var signUpBtn = document.getElementById("signup-btn");
 var signInBtn = document.getElementById("signin-btn");
+var signUpLink = document.getElementById("signup-link");
+
+// Get close buttons
+var closeSignUp = document.getElementById("close-signup");
+var closeSignIn = document.getElementById("close-signin");
 
 // Open the modal
 openModal = modal => {
@@ -19,3 +24,12 @@ closeModal = modal => {
 // Event to handle the click on Signin/signup buttons
 signInBtn.addEventListener("click", () => openModal(signInModal));
 signUpBtn.addEventListener("click", () => openModal(signUpModal));
+
+// Event to handle close button
+closeSignUp.addEventListener("click", () => closeModal(signUpModal));
+closeSignIn.addEventListener("click", () => closeModal(signInModal));
+
+signUpLink.onclick = function() {
+    closeModal(signInModal);
+    openModal(signUpModal);
+};
